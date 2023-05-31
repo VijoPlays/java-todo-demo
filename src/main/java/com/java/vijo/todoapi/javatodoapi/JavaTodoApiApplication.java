919d706068e2aa20bfde326542e171820383fab5
Alexander Ott <blfasan@yahoo.de>
2023-05-31 07:54:46 +0200

    Project setup

---
+++ src/main/java/com/java/vijo/todoapi/javatodoapi/JavaTodoApiApplication.java
@@ -0,0 +1,13 @@
+package com.java.vijo.todoapi.javatodoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTodoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTodoApiApplication.class, args);
+	}
+
+}
